f9ef7e912c699601c5adf553cf36d40ea9ccbabd
Osvbaa <osva.bav@gmail.com>
2026-02-26 10:54:59 -0600

    feat: add stickyHeader to Task list for better organization

---
+++ app/src/main/java/com/example/dailyfocus/data/model/StatItem.kt
@@ -0,0 +1,4 @@
+package com.example.dailyfocus.data.model
+
+class StatItem {
+}
--- app/src/main/java/com/example/dailyfocus/ui/screens/TaskScreen.kt
+++ app/src/main/java/com/example/dailyfocus/ui/screens/TaskScreen.kt
@@ -1,7 +1,8 @@
 package com.example.dailyfocus.ui.screens
 
-import android.preference.PreferenceActivity
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -16,16 +17,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.dailyfocus.ui.components.TaskCard
-import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import com.example.dailyfocus.data.model.Task
+import kotlin.collections.groupBy
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -38,50 +41,90 @@ fun TaskScreen(modifier : Modifier = Modifier) {
             Task(title = "Aprender Inglés", description = "Practicar nuevo vocabulario"),
             Task(title = "Practicar speaking", description = "Hablar por 5 minutos en inglés"),
             Task(title = "Aprender a utilizar LazyLists"),
-            Task(title = "Hacer ejercicio", isCompleted = true)
+            Task(title = "Hacer tarea", isCompleted = true),
+            Task(title = "Estudiar Android", description = "Practicar conceptos nuevos"),
+            Task(title = "Leer", description = "Leer libros por 5 minutos en inglés"),
+            Task(title = "Trabajar sobre Daily Focus"),
+            Task(title = "Limpiar el cuarto", isCompleted = true)
         )
     }
 
+    val groupedTasks by remember {
+        derivedStateOf {
+            tasks.groupBy { task ->
+                // Formateamos la fecha a String para usarla como separador visual
+                task.createdAt.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+            }
+        }
+    }
+
     Scaffold (modifier = modifier.fillMaxSize()) { innerPadding ->
-        LazyColumn(
-            modifier = Modifier.padding(paddingValues = innerPadding),
-            contentPadding = PaddingValues(all = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(space = 16.dp)
-        ) {
-            item {
-                HeaderSection()
-                Spacer(modifier = Modifier.height(height = 16.dp))
+        HeaderSection(
+            groupedTasks = groupedTasks,
+            onTaskCheckedChange = { task, isChecked ->
+                val index = tasks.indexOfFirst { it.id == task.id }
+                if (index != -1) {
+                    tasks[index] = tasks[index].copy(isCompleted = isChecked)
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+@Composable
+private fun HeaderSection(
+    groupedTasks: Map<String, List<Task>>,
+    onTaskCheckedChange: (Task, Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(all = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(space = 8.dp)
+    ) {
+        item {
+            Spacer(modifier = Modifier.height(height = 12.dp))
+            Text(text = "Daily Focus", style = MaterialTheme.typography.headlineLarge)
+            Spacer(modifier = Modifier.height(height = 16.dp))
+        }
+
+        groupedTasks.forEach { (dateHeader, tasksForDate) ->
+            stickyHeader(
+                key = "header_$dateHeader", // Identidad única para la Slot Table
+                contentType = "header_type" // Molde para reciclaje en memoria
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(color = MaterialTheme.colorScheme.primary)
+                        .padding(all = 8.dp)
+                ) {
+                    Text(
+                        text = dateHeader,
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onPrimary,
+                    )
+                }
             }
 
             items(
-                items = tasks,
+                items = tasksForDate,
                 key = { task -> task.id },
-                contentType = { "task_item "}
+                contentType = { "task_item" }
             ) { task ->
                 TaskCard(
                     task = task,
-                    onCheckedChange = { isChecked ->
-                        val index = tasks.indexOfFirst { it.id == task.id }
-                        if (index != -1) {
-                            tasks[index] = tasks[index].copy(isCompleted = isChecked)
-                        }
-                    },
-                    modifier = Modifier.fillMaxWidth()
+                    onCheckedChange = { newState ->
+                        onTaskCheckedChange(task, newState)
+                    }
                 )
             }
         }
     }
 }
+
+@Preview
 @Composable
-fun HeaderSection() {
-    Column {
-        Text(
-            text = "Daily Focus",
-            style = MaterialTheme.typography.headlineLarge
-        )
-        Text(
-            text = "Hoy es ${LocalDate.now()}",
-            style = MaterialTheme.typography.bodyLarge
-        )
-    }
+fun TaskScreenPreview() {
+    TaskScreen()
 }
