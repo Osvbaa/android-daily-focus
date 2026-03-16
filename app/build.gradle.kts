03a89d9413aeaf61d87bb4812462572aac130de7
Osvbaa <osva.bav@gmail.com>
2026-03-16 17:26:23 -0600

    chore: correct a word mistake in libs.version.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("androidx.navigation:navigation-compose:2.8.0") // Navegación en Compose
-    implementation("org.jetbrains.kotlix:kotlinx-serialization-json:1.6.3") // Motor de Serialización de Kotlin
+    implementation(libs.androidx.navigation.compose) // Navegación en Compose
+    implementation(libs.kotlinx.serialization.json) // Motor de Serialización de Kotlin
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
