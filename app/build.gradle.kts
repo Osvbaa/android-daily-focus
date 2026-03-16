fc67076478fda74400102a55ee31e7c1a4d43f8a
Osvbaa <osva.bav@gmail.com>
2026-03-16 09:38:43 -0600

    chore: add Kotlin Serialization dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization")
 }
 
 android {
@@ -51,6 +52,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.navigation:navigation-compose:2.8.0") // Navegación en Compose
+    implementation("org.jetbrains.kotlix:kotlinx-serialization-json:1.6.3") // Motor de Serialización de Kotlin
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
