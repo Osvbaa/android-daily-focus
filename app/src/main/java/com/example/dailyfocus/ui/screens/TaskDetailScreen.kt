23843efa0179405e21d67d1a98154bf916f3f0a4
Osvbaa <osva.bav@gmail.com>
2026-04-17 13:31:15 -0600

    refactor: create detail task screen

--- app/src/main/java/com/example/dailyfocus/ui/screens/TaskDetailScreen.kt
+++ app/src/main/java/com/example/dailyfocus/ui/screens/TaskDetailScreen.kt
@@ -1,24 +1,45 @@
 package com.example.dailyfocus.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-
+import androidx.compose.ui.unit.dp
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TaskDetailScreen(taskId: String, onNavigateBack: () -> Unit) {
+fun TaskDetailScreen(
+    title: String,
+    description: String,
+    isCompleted: Boolean,
+    isSaveEnabled: Boolean, // <--- Nueva propiedad
+    onValueChangeTitle: (String) -> Unit,
+    onValueChangeDescription: (String) -> Unit,
+    onToggleCompleted: (Boolean) -> Unit,
+    onSaveTaskChanges: () -> Unit,
+    onNavigateBack: () -> Unit
+) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -34,14 +55,68 @@ fun TaskDetailScreen(taskId: String, onNavigateBack: () -> Unit) {
             )
         }
     ) { padding ->
-        Box(
-            modifier = Modifier.padding(paddingValues = padding).fillMaxSize(),
-            contentAlignment = Alignment.Center
+        Column(
+            modifier = Modifier
+                .padding(paddingValues = padding)
+                .fillMaxSize()
+                .verticalScroll(
+                    state = rememberScrollState(),
+                    enabled = true
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             Text(
-                text = "Detalles de la tarea: $taskId",
-                style = MaterialTheme.typography.headlineSmall
+                text = "Editar tarea",
+                style = MaterialTheme.typography.titleLarge
             )
+            // Título editable
+            OutlinedTextField(
+                value = title, // Vendría del estado del ViewModel
+                onValueChange = onValueChangeTitle,
+                label = { Text("Título") },
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .fillMaxWidth()
+            )
+
+            // Descripción editable
+            OutlinedTextField(
+                value = description,
+                onValueChange = onValueChangeDescription,
+                label = { Text("Descripción") },
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .fillMaxWidth()
+                    .height(150.dp),
+                minLines = 3
+            )
+
+            // Selector de estado
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text("¿Tarea completada?",
+                    modifier = Modifier.weight(1f))
+                Switch(checked = isCompleted, onCheckedChange = onToggleCompleted)
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            // Botón de guardar cambios
+            Button(
+                onClick = onSaveTaskChanges,
+                enabled = isSaveEnabled, // <--- Conectado aquí
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .fillMaxWidth(),
+                shape = MaterialTheme.shapes.medium
+            ) {
+                Text("Guardar Cambios")
+            }
         }
     }
 }
